5831150d6bc5b17e74317533ac6ea984902e2fbc
sonrossy <tristesse0889@hotmail.com>
2018-08-02 14:18:54 -0400

    added isSalaryHigher() method

--- test/PayRoll.java
+++ test/PayRoll.java
@@ -4,9 +4,22 @@
  * @author tristesse08
  */
 public class PayRoll {
+    private Employee tome=new Employee("Tom Cruise",8888,32000.0,"YEARLY","9/16/18","Full-time",45);
+
+    
+    public static void calculatePay(Employee current){
+        if(current.payRate=="YEARLY"){
+            
+        }
+    }
+    
     public static void main(String[] args) {
-        Employee tom=new Employee("Tom Cruise",8888,32000.0,"$16/h","9/16/18","Assoiciate",45);
+        Employee tom=new Employee("Tom Cruise",8888,32000.0,"YEARLY","9/16/18","Full-time",45);
+        Employee ron=new Employee("Ron Civil",18,"HOURLY","9/16/18","Part-time");
         System.out.println(tom);
+        System.out.printf("%3d",23.223);
+        
+
     }
     
 }
